2a35963b68467fe8e3eeb4964627fefae46998dd
noah <garciasharrettsnoah@gmail.com>
2022-06-07 18:13:37 -0700

    working spawing grids at intervals

--- index.js
+++ index.js
@@ -123,11 +123,11 @@ class Invader {
     )
   }
 
-  update() {
+  update({ velocity }) {
     if (this.image) {
       this.draw()
-      this.position.x +=  this.velocity.x
-      this.position.y +=  this.velocity.y
+      this.position.x +=  velocity.x
+      this.position.y +=  velocity.y
 
       
     } 
@@ -142,7 +142,7 @@ class Grid {
     }
 
     this.velocity = {
-      x: 0,
+      x: 3,
       y: 0
     }
 
@@ -150,6 +150,9 @@ class Grid {
 
     const columns = Math.floor(Math.random() * 10 + 5)
     const rows = Math.floor(Math.random() * 5 + 2)
+
+    this.width = columns * 50
+
     for(let x = 0; x < columns; x++) {
       for(let y = 0; y < rows; y++) {
         this.invaders.push(
@@ -165,7 +168,15 @@ class Grid {
   }
 
   update() {
+    this.position.x += this.velocity.x
+    this.position.y += this.velocity.y
 
+    this.velocity.y = 0
+
+    if (this.position.x + this.width >= canvas.width  || this.position.x <= 0) {
+      this.velocity.x = -this.velocity.x
+      this.velocity.y = 30
+    }
   }
 }
 
@@ -186,6 +197,7 @@ const keys = {
   }
 }
 
+let frames = 0
 
 function animate() {
   requestAnimationFrame(animate)
@@ -206,7 +218,7 @@ function animate() {
   grinds.forEach(grid => {
     grid.update()
     grid.invaders.forEach(invader => {
-      invader.update()
+      invader.update({ velocity: grid.velocity })
     })
   })
 
@@ -221,7 +233,11 @@ function animate() {
     player.rotation = 0
   }
 
-  
+  if (frames % 1000 === 0) {
+   grinds.push(new Grid())
+  }
+
+  frames++
 }
 
 animate()
